55cbc40ac261301fa28c69f3eb884ce5007dae17
enderst3 <enderst3@gmail.com>
2020-02-11 20:14:54 -0800

    armstrong-numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,10 +1,16 @@
 def is_armstrong_number(number):
-    # create list to store numbers to add
-    digits = []
-    # loop through digits in number and multiply by lenth, then add to list
-    for d in str(number):
-        digits.append(int(d)**len(str(number)))
+    # create list of digits mulitiplied by the power of len.
+    digits = [int(d)**len(str(number)) for d in str(number)]
+    
     # Check if the two numbers equal each other
     if sum(digits) != number:
         return False
     return True
+
+'''
+# as a for loop it would be 
+digits = []
+for d in str(number):
+    digits.append(int(d)**len(str(number)))
+
+'''
